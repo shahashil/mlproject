c22728daf6d5927f247ad41936f760041e33e74c
Ashil <ashil.s@indianic.com>
2023-07-05 16:13:42 +0530

    Video 2

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ def get_requirements(file_path:str) -> List[str]:
     '''
     this function will return the list of requriements
     '''
+    HYPHEN_E = "-e ."
     all_requirements = []
     with open(file_path) as f_obj:
         all_requirements = f_obj.readlines()
 
     all_requirements = [ file.replace("\n","") for file in all_requirements ]
+
+    if HYPHEN_E in all_requirements:
+        all_requirements.remove(HYPHEN_E)
+
+    return all_requirements
 
 
 
