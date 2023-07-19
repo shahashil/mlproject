f128476b8fc00fa2ef14bc3251757142af84ea88
Ashil <ashil.s@indianic.com>
2023-07-19 13:31:49 +0530

    video 8

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,14 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 #from src.exception import CustomException
 from exception import CustomException
 from logger import logging
-from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -20,15 +21,31 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)        
+
+
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for model_name in models.keys():
             model = models[model_name]
+            para=param[model_name]
             
-            model.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)                        
+            # model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred  = model.predict(X_test)
